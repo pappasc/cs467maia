f0bc0d4a538ddbf13a82bfd862602e8bb42eb13e
pappasc <pappasc@oregonstate.edu>
2019-04-24 11:27:44 -0700

    Homepage now has control structures for rendering different structures based on login attributes

--- employee-recognition-site/server.js
+++ employee-recognition-site/server.js
@@ -12,14 +12,16 @@ const path = require('path');
 const uuid = require('uuid/v4')
 const session = require('express-session')
 const {Datastore} = require('@google-cloud/datastore');
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
 const app = express();
 const DatastoreStore = require('@google-cloud/connect-datastore')(session);
+
+//Enable requests with bodies to be in JSON format
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//Enable sessions and instantiate store for cookies
 app.use(session({
     genid: (req) => {
 	return uuid();
@@ -36,6 +38,9 @@ app.use(session({
     saveUninitialized: true
 }));
 
+//Enable a local strategy for logging in
+//Sift through users first then admins
+//Adds a "type" attribute to objects to distinguish users from admins
 passport.use(new LocalStrategy((username, password, done) => {
     //logic for checking user credentials
     //make call to DB API to retreive user password and id
@@ -62,6 +67,7 @@ passport.use(new LocalStrategy((username, password, done) => {
 	    
 	    return rp(options2).then(function(passObj) {
 		if (passObj.password == password) {
+		    userObj.type = 'user';
 		    return done(null, userObj);
 		}
 		else {
@@ -70,34 +76,116 @@ passport.use(new LocalStrategy((username, password, done) => {
 	    });
 	}
 	else {
-	    return done(null, false);
+	    var options3 = {
+		uri: 'http://localhost:8080/admins',
+		json: true // Automatically parses the JSON string in the response
+	    };
+	    
+	    return rp(options3).then(function (admins) {
+		var j;
+		var adminObj = null;
+		for (j = 0; j < admins.admin_ids.length; j++) {
+		    if (username == admins.admin_ids[j].email_address) {
+			adminObj = admins.admin_ids[j];
+			break;
+		    }
+		}
+		if (adminObj != null) {
+		    var options4 = {
+			uri: 'http://localhost:8080/admins/' + adminObj.admin_id + '/login',
+			json: true
+		    };
+		    
+		    return rp(options4).then(function(passObj) {
+			if (passObj.password == password) {
+			    adminObj.type = 'admin';
+			    return done(null, adminObj);
+			}
+			else {
+			    return done(null, false);
+			}
+		    });
+		}
+		else {
+		    return done(null, false);
+		}
+	    });
 	}
     });
 }));
 
+//Store necessary info about the user on the server for later retreival
 passport.serializeUser((user, done) => {
-    done(null, user.user_id);
+    if (user != false) {
+	var userIdent = {
+	    type: user.type
+	};
+	if (user.type == 'admin') {
+	    userIdent.admin_id = user.admin_id;
+	}
+	else {
+	    userIdent.user_id = user.user_id;
+	}
+	done(null, userIdent);
+    }
+    else {
+	done(null, user);
+    }
 });
 
-passport.deserializeUser((user_id, done) => {
+//Use the serialized user info to retreive user from database
+passport.deserializeUser((userIdent, done) => {
     //logic for checking user id
     //should make a call to DB API to retreive id info
-
+    var uriType;
+    if (userIdent.type == 'user') {
+	uriType = 'users';
+    }
+    else if (userIdent.type == 'admin') {
+	uriType = 'admins';
+    }
+    else {
+	done(null, false);
+    }
+    
     var options = {
-	uri: 'http://localhost:8080/users',
+	uri: 'http://localhost:8080/' + uriType,
 	json: true // Automatically parses the JSON string in the response
     };
     
-    rp(options).then(function (users) {
+    rp(options).then(function (accounts) {
 	var i;
-	var userObj = false;
-	for (i = 0; i < users.user_ids.length; i++) {
-	    if (user_id == users.user_ids[i].user_id) {
-		userObj = users.user_ids[i];
-		break;
+	var accountObj = null;
+	if (userIdent.type == 'user') {
+	    for (i = 0; i < accounts.user_ids.length; i++) {
+		if (userIdent.user_id == accounts.user_ids[i].user_id) {
+		    accountObj = accounts.user_ids[i];
+		    accountObj.type = 'user';
+		    break;
+		}
+	    }
+
+	}
+	else if (userIdent.type == 'admin') {
+	    for (i = 0; i < accounts.admin_ids.length; i++) {
+		if (userIdent.admin_id == accounts.admin_ids[i].admin_id) {
+		    accountObj = accounts.admin_ids[i];
+		    accountObj.type = 'admin';
+		    break;
+		}
 	    }
 	}
-	done(null, userObj);
+	else {
+	    done(null, false);
+	}
+
+	if (accountObj != null) {
+	    done(null, accountObj);
+	}
+	
+	else {
+	    done(null, false);
+	}
     });
 });
 
@@ -111,7 +199,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/users', require('./testCode/dummyBackend.js'));
+//Dummy backend for now. Will port to different module later
+app.use('/users', require('./testCode/userProxyBackend.js'));
+app.use('/admins', require('./testCode/adminProxyBackend.js'));
 
 app.get('/', function (req, res) {
     //Redirect straight to home
@@ -121,12 +211,21 @@ app.get('/', function (req, res) {
 app.get('/home', function (req, res) {
     //Display a homepage with a link to start the process
     var context = {};
-    context.login = "";
     if (req.isAuthenticated()) {
-	context.login = req.user.first_name + req.user.last_name;
+	context.login = false;
+	if (req.user.type == 'user') {
+	    context.isUser = true;
+	    context.isAdmin = false;
+	    context.name = 'User ' + req.user.first_name + ' ' + req.user.last_name;
+	}
+	else {
+	    context.isUser = false;
+	    context.isAdmin = true;
+	    context.name = 'Admin ' + req.user.first_name + ' ' + req.user.last_name;
+	}
     }
     else {
-	context.login = "Need to be logged in";
+	context.login = true;
     }
     res.render('homepage', context);
 });
